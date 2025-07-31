fa328ce11047d4672cc74ae87eab34110c7d1ffd
m1nhpham2k4 <minh02122004@gmail.com>
2025-07-31 14:41:44 +0700

    update

--- Chatbot/app/dbs.py
+++ Chatbot/app/dbs.py
@@ -1,16 +1,14 @@
 from pymongo import MongoClient
 import gridfs
-import certifi
 from config import MONGODB_URI
 
-# Bổ sung các đối số tls=True và tlsCAFile để dùng chứng chỉ đáng tin cậy
-client = MongoClient(MONGODB_URI, tls=True, tlsCAFile=certifi.where())
+client = MongoClient(MONGODB_URI)
 
 try:
     client.admin.command("ping")
-    print("MongoDB connected successfully")
+    print("MongoDB connected succesfully")
 except Exception as e:
-    print('MongoDB connection failed:', e)
+    print('MongoDB connection failed:',e)
 
 db = client["vqa_project"]
 fs = gridfs.GridFS(db)
