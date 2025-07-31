72d9127214bbbfea98b289e171fd74b7bfc7674b
m1nhpham2k4 <minh02122004@gmail.com>
2025-07-31 14:17:49 +0700

    temp: save before rebase

--- Chatbot/app/config.py
+++ Chatbot/app/config.py
@@ -1,10 +1,19 @@
 import os
 from dotenv import load_dotenv
 
+# Load biến môi trường từ .env
 load_dotenv()
 
-GEMINI_API_KEY= os.getenv("GEMINI_API_KEY")
-MONGODB_USERNAME=os.getenv("MONGODB_USERNAME")
-MONGODB_PASSWORD=os.getenv("MONGODB_PASSWORD")
+# Lấy các biến
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+MONGODB_USERNAME = os.getenv("MONGODB_USERNAME")
+MONGODB_PASSWORD = os.getenv("MONGODB_PASSWORD")
 
-MONGODB_URI = f'mongodb+srv://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@cluster0.s84nghm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+# In ra kiểm tra
+# print("GEMINI_API_KEY:", GEMINI_API_KEY)
+# print("MONGODB_USERNAME:", MONGODB_USERNAME)
+# print("MONGODB_PASSWORD:", MONGODB_PASSWORD)
+
+# Tạo URI
+MONGODB_URI = f'mongodb+srv://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@cluster0.s84nghm.mongodb.net/'
+# print("MONGODB_URI:", MONGODB_URI)
--- Chatbot/app/dbs.py
+++ Chatbot/app/dbs.py
@@ -1,14 +1,16 @@
 from pymongo import MongoClient
 import gridfs
+import certifi
 from config import MONGODB_URI
 
-client = MongoClient(MONGODB_URI)
+# Bổ sung các đối số tls=True và tlsCAFile để dùng chứng chỉ đáng tin cậy
+client = MongoClient(MONGODB_URI, tls=True, tlsCAFile=certifi.where())
 
 try:
     client.admin.command("ping")
-    print("MongoDB connected succesfully")
+    print("MongoDB connected successfully")
 except Exception as e:
-    print('MongoDB connection failed:',e)
+    print('MongoDB connection failed:', e)
 
 db = client["vqa_project"]
 fs = gridfs.GridFS(db)
